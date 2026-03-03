e8711721215d180677c2ff1131055294f7c83ba7
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-03-03 16:40:43 +0100

    Utilities to apply the Glasbey LUTs on an image.

--- src/main/java/fiji/plugin/appose/ApposeUtils.java
+++ src/main/java/fiji/plugin/appose/ApposeUtils.java
@@ -1,6 +1,8 @@
 package fiji.plugin.appose;
 
 import ij.ImagePlus;
+import ij.plugin.LutLoader;
+import ij.process.LUT;
 import net.imagej.ImgPlus;
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.type.numeric.real.DoubleType;
@@ -18,5 +20,17 @@ public class ApposeUtils
 		final ImgPlus< DoubleType > img = ImagePlusAdapter.wrapImgPlus( imp );
 		final ImgPlus raw = img;
 		return raw;
+	}
+
+	public static final void useGlasbeyDarkLUT( final ImagePlus imp)
+	{
+		final LUT lut = LutLoader.openLut( ApposeUtils.class.getResource( "/glasbey_on_dark.lut" ).getFile() );
+		useLUT( imp, lut );
+	}
+
+	public static final void useLUT( final ImagePlus imp, final LUT lut )
+	{
+		imp.setLut( lut );
+		imp.updateAndDraw();
 	}
 }
