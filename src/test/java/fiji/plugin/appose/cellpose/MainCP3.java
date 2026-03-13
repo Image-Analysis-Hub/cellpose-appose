d23a7cc39803809afa4b0c453fb112c2f28d7bdf
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-03-13 13:45:19 +0100

    Update license blurbs

--- src/test/java/fiji/plugin/appose/cellpose/MainCP3.java
+++ src/test/java/fiji/plugin/appose/cellpose/MainCP3.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * Running Cellpose with a Fiji plugin based on Appose.
+ * %%
+ * Copyright (C) 2026 My Company, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 package fiji.plugin.appose.cellpose;
 
 import fiji.plugin.appose.cellpose.cp3.CellposeAppose;
