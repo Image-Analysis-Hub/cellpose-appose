b213d335488b85c34a56a852e6b0c018a3b0551d
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-04-28 16:52:00 +0200

    Create ImageAxisInfo from an ImgPlus.

--- src/main/java/fiji/plugin/appose/ImageAxisInfo.java
+++ src/main/java/fiji/plugin/appose/ImageAxisInfo.java
@@ -21,6 +21,8 @@
  */
 package fiji.plugin.appose;
 
+import net.imagej.ImgPlus;
+
 public class ImageAxisInfo
 {
 	public final Integer z_axis;
@@ -29,10 +31,38 @@ public class ImageAxisInfo
 
 	public final Integer time_axis;
 
-	public ImageAxisInfo( Integer z_axis, Integer channel_axis, Integer time_axis )
+	public ImageAxisInfo( final Integer z_axis, final Integer channel_axis, final Integer time_axis )
 	{
 		this.z_axis = z_axis;
 		this.channel_axis = channel_axis;
 		this.time_axis = time_axis;
+	}
+
+	public static ImageAxisInfo fromImgPlus( final ImgPlus< ? > img )
+	{
+		Integer zAxis = null;
+		Integer channelAxis = null;
+		Integer timeAxis = null;
+
+		for ( int i = 0; i < img.numDimensions(); i++ )
+		{
+			final String axisType = img.axis( i ).type().toString().trim();
+			switch ( axisType )
+			{
+			case "Z":
+				zAxis = i;
+				break;
+			case "Channel":
+				channelAxis = i;
+				break;
+			case "Time":
+				timeAxis = i;
+				break;
+			default:
+				// Ignore other axes
+			}
+		}
+
+		return new ImageAxisInfo( zAxis, channelAxis, timeAxis );
 	}
 }
