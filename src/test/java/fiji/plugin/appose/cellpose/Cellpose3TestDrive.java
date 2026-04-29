9e247d349786c90e22adc9495d0a6b80d4471608
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-04-29 16:10:12 +0200

    Tweak demo.

--- src/test/java/fiji/plugin/appose/cellpose/Cellpose3TestDrive.java
+++ src/test/java/fiji/plugin/appose/cellpose/Cellpose3TestDrive.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import org.apposed.appose.BuildException;
 import org.apposed.appose.TaskException;
 
+import fiji.plugin.appose.ApposeUtils;
 import fiji.plugin.appose.cellpose.cp3.Cellpose3;
+import fiji.plugin.appose.cellpose.cp3.Cellpose3Model;
 import fiji.plugin.appose.cellpose.cp3.Cellpose3Parameters;
 import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
-import net.imagej.ImageJ;
+import ij.plugin.frame.RoiManager;
 
 /**
  * Interactive tests drive for Cellpose 3, using the static methods.
@@ -19,12 +22,28 @@ public class Cellpose3TestDrive
 
 	public static void main( final String[] args ) throws BuildException, IOException, InterruptedException, TaskException
 	{
-		final ImageJ ij = new ImageJ();
-		ij.launch();
-		final ImagePlus imp = IJ.openImage( "http://imagej.net/images/blobs.gif" );
+		try
+		{
+			ImageJ.main( args );
+			final ImagePlus imp = IJ.openImage( "http://imagej.net/images/blobs.gif" );
 
-		final Cellpose3Parameters params = Cellpose3Parameters.defaultCytoParameters();
-		final ImagePlus[] output = Cellpose3.run( imp, params );
-		output[ 0 ].show();
+//			final Cellpose3Parameters params = Cellpose3Parameters.defaultCytoParameters();
+			final Cellpose3Parameters params = Cellpose3Parameters.builder()
+					.model( Cellpose3Model.CYTO2 )
+					.diameter( 30 )
+					.build();
+
+			final ImagePlus[] output = Cellpose3.run( imp, params );
+			final ImagePlus labels = output[ 0 ];
+
+			imp.show();
+			ApposeUtils.addROIs( labels, "Cellpose" );
+			RoiManager.getInstance2().runCommand( "Show All" );
+			labels.show();
+		}
+		catch ( final Exception e )
+		{
+			e.printStackTrace();
+		}
 	}
 }
