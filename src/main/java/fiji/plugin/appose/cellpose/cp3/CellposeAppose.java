458a18c62e34bb3dfac4bf1a4814f9070b4670de
Gaelle Letort <gaelle.letort.gl@gmail.com>
2026-04-30 17:51:25 +0200

    set defaut cellpob threshold to 0

--- src/main/java/fiji/plugin/appose/cellpose/cp3/CellposeAppose.java
+++ src/main/java/fiji/plugin/appose/cellpose/cp3/CellposeAppose.java
@@ -148,6 +148,8 @@ public class CellposeAppose extends DynamicCommand implements Initializable
 	@Override
 	public void initialize()
 	{
+		// set the default value, otherwise it gets to -6
+		setInput( "cellprob_threshold", 0.0) ;
 		// Grab the current image (last touched image in Fiji)
 		final ImagePlus imp = WindowManager.getCurrentImage();
 		if ( imp == null )
--- src/main/java/fiji/plugin/appose/cellpose/cp4/CellposeSAMAppose.java
+++ src/main/java/fiji/plugin/appose/cellpose/cp4/CellposeSAMAppose.java
@@ -140,6 +140,8 @@ public class CellposeSAMAppose extends DynamicCommand implements Initializable
 	@Override
 	public void initialize()
 	{
+		// set the default value, otherwise it gets to -6
+		setInput( "cellprob_threshold", 0.0) ;
 		// Grab the current image (last touched image in Fiji)
 		final ImagePlus imp = WindowManager.getCurrentImage();
 		if ( imp == null )
