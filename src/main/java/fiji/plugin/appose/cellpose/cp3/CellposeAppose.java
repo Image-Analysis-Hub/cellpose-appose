4d52fd8344ce3ed70f45ab6d3cfd4aee9ccb5ac4
Gaelle Letort <34513249+gletort@users.noreply.github.com>
2026-06-03 12:06:56 +0200

    handle if roi is null and parameter to normal

--- src/main/java/fiji/plugin/appose/cellpose/cp3/CellposeAppose.java
+++ src/main/java/fiji/plugin/appose/cellpose/cp3/CellposeAppose.java
@@ -152,18 +152,18 @@ public class CellposeAppose extends DynamicCommand implements Initializable
 	private Boolean compute_flows = false; // whether to compute flows channel
 
 	// ---------
-	@Parameter(visibility=ItemVisibility.MESSAGE, label="<html><b>3D Options</b></html>", persist = false)
+	@Parameter( visibility=ItemVisibility.MESSAGE, label="<html><b>3D Options</b></html>", persist = false)
     private final String dimMsg = "<html><hr width='100'></html>";
 
-	@Parameter( label = "Mode 3D", choices = { "None", "2D+stitch", "3D" }, description = "Mode of 3D segmentation" )
+	@Parameter( visibility=ItemVisibility.NORMAL, label = "Mode 3D", choices = { "None", "2D+stitch", "3D" }, description = "Mode of 3D segmentation" )
 	private String mode_3d = "None"; // mode 3D of CP to use, only for 3D image
 
 	private boolean is3D = false;
 
-	@Parameter( label="Stitch threshold", min="0.0", max="1.0", description="2D+stitch mode only: IOU threshold to stitch labels together along the Z-axis" )
+	@Parameter( visibility=ItemVisibility.NORMAL, label="Stitch threshold", min="0.0", max="1.0", description="2D+stitch mode only: IOU threshold to stitch labels together along the Z-axis" )
 	private Double stitch_threshold = 0.1; 
 	
-	@Parameter( label="Flow3d smooth", min="0", description="3D mode only: Gaussian smoothing sigma applied on flows." ) 
+	@Parameter( visibility=ItemVisibility.NORMAL, label="Flow3d smooth", min="0", description="3D mode only: Gaussian smoothing sigma applied on flows." ) 
 	private Integer flow3d_smooth = 0; // gaussian smooth of 3D flows
 	
 
