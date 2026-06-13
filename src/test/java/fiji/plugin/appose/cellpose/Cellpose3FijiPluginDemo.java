a020d6231dcaa31995fbc2a671a3952e4f733dff
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-06-13 12:00:00 +0200

    Demo the Cellpose 3 ImageJ plugin.

---
+++ src/test/java/fiji/plugin/appose/cellpose/Cellpose3FijiPluginDemo.java
@@ -0,0 +1,36 @@
+package fiji.plugin.appose.cellpose;
+
+import fiji.plugin.appose.cellpose.cp3.Cellpose3Plugin;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.plugin.frame.Recorder;
+
+public class Cellpose3FijiPluginDemo
+{
+	@SuppressWarnings( "unchecked" )
+	public static void main( final String[] args )
+	{
+		try
+		{
+			ImageJ.main( args );
+
+			// Since this is a demo in the src folder, we need to register the
+			// plugin manually, as it won't be picked up by the usual plugin
+			// discovery mechanism.
+			ij.Menus.getCommands().put( "Cellpose 3", "fiji.plugin.appose.cellpose.cp3.Cellpose3Plugin" );
+
+			// Switch on macro recorder.
+			new Recorder();
+
+			final ImagePlus imp = IJ.openImage( "http://imagej.net/images/blobs.gif" );
+			imp.show();
+
+			new Cellpose3Plugin().run( "" );
+		}
+		catch ( final Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+}
