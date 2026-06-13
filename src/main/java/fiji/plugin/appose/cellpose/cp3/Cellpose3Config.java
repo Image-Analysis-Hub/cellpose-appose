4325ae00ddaee2df8978c10f6f87b31d5d84e199
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-06-13 11:57:52 +0200

    The Configurator for the Cellpose 3 algo.
    Re-implements all the parameters initially used in the
    Cellpose-Appose plugin, except for resample.
    We must have resample set to true, ottherwise the dimensions
    of the output image does not match the input's.

---
+++ src/main/java/fiji/plugin/appose/cellpose/cp3/Cellpose3Config.java
@@ -0,0 +1,383 @@
+package fiji.plugin.appose.cellpose.cp3;
+
+import org.scijava.ui.config.Configurator;
+import org.scijava.ui.config.Parameters.BooleanParam;
+import org.scijava.ui.config.Parameters.ChoiceParam;
+import org.scijava.ui.config.Parameters.DoubleParam;
+import org.scijava.ui.config.Parameters.EnumParam;
+import org.scijava.ui.config.Parameters.IntParam;
+import org.scijava.ui.config.Parameters.PathParam;
+
+import net.imglib2.cellpose.Cellpose3BuiltinModels;
+
+public class Cellpose3Config extends Configurator
+{
+
+	private final EnumParam< Cellpose3BuiltinModels > builtinModel;
+
+	private final PathParam customModel;
+
+	private final SelectableParameters builtinOrCustom;
+
+	private final IntParam chan1;
+
+	private final IntParam chan2;
+
+	private final DoubleParam diameter;
+
+	private final DoubleParam flowThreshold;
+
+	private final DoubleParam cellprobThreshold;
+
+	private final IntParam minSize;
+
+	private final BooleanParam exportROIs;
+
+	private final BooleanParam exportLabels;
+
+	private final BooleanParam exportFlows;
+
+	private final BooleanParam normalize;
+
+	private final IntParam nIter;
+
+	private final BooleanParam do3Dseg;
+
+	private final DoubleParam stitchThreshold;
+
+	private final IntParam flow3DSmooth;
+
+	private final DoubleParam tileOverlap;
+
+	private final BooleanParam useGpu;
+
+	private final ChoiceParam torchVersion;
+
+	public Cellpose3Config( final int nChannels, final double pixelSize, final String units )
+	{
+		super( "Cellpose 3", "https://imagej.net/plugins/cellpose-appose#usage" );
+
+		// Choice among an enum.
+		this.builtinModel = addEnumParameter( Cellpose3BuiltinModels.class )
+				.key( "BUILTIN_MODEL" )
+				.defaultValue( Cellpose3BuiltinModels.CYTO3 )
+				.name( "Builtin model" )
+				.help( "https://cellpose.readthedocs.io/en/v3.1.1.1/models.html#full-built-in-models" )
+				.get();
+
+		// File path.
+		this.customModel = addPathParameter()
+				.key( "CUSTOM_MODEL_PATH" )
+				.defaultValue( "" ) // Better than null.
+				.name( "Path to custom model" )
+				.help( "Path to a custom Cellpose 3 model. " )
+				.get();
+
+		// One or the other, but not both.
+		this.builtinOrCustom = addSelectableParameters()
+				.key( "BUILTIN_OR_CUSTOM" )
+				.add( builtinModel )
+				.add( customModel )
+				.get();
+
+		// Channels, two int params.
+		this.chan1 = addIntParameter()
+				.key( "CHAN1" )
+				.name( "Main channel" )
+				.help( "The main channel to segment. Select 0 to use a grayscale blend of all channels." )
+				.defaultValue( 1 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+		this.chan2 = addIntParameter()
+				.key( "CHAN2" )
+				.name( "Optional channel" )
+				.help( "The second channel to segment. Select 0 to skip using a second channel." )
+				.defaultValue( 0 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+
+		// Diameter param is in pixel, but we want to display it in physical
+		// units. So we set a translator that converts between the two.
+
+		this.diameter = addDoubleParameter()
+				.key( "DIAMETER" )
+				.name( "Diameter" )
+				.help( "<html>Estimated diameter of objects, in physical units "
+						+ "(stored in pixel size internally). " +
+						"Set to 0 to let Cellpose estimate it automatically.</html>" )
+				.units( units )
+				.defaultValue( 30. )
+				.min( 0. ) // But no max
+				.get();
+
+		setDisplayTranslator( diameter, d -> d * pixelSize, d -> d / pixelSize );
+
+		/*
+		 * Advanced parameters.
+		 */
+
+		this.flowThreshold = addDoubleParameter()
+				.key( "FLOW_THRESHOLD" )
+				.name( "Flow threshold" )
+				.help( "<html>Threshold for flow error filtering. Lower = more masks (permissive), Higher = fewer masks (strict).</html>" )
+				.defaultValue( 0.4 )
+				.min( 0. )
+				.max( 3. )
+				.get();
+
+		this.cellprobThreshold = addDoubleParameter()
+				.key( "CELLPROB_THRESHOLD" )
+				.name( "Cell probability threshold" )
+				.help( "<html>Threshold for cell probability. Increase to filter low-confidence detections.</html>" )
+				.defaultValue( 0.0 )
+				.min( -6. )
+				.max( 6. )
+				.get();
+
+		this.minSize = addIntParameter()
+				.key( "MIN_SIZE" )
+				.name( "Minimum size" )
+				.help( "Objects smaller than this are removed." )
+				.defaultValue( 15 )
+				.min( 0 )
+				.units( "pixels" )
+				.get();
+
+		this.normalize = addBooleanParameter()
+				.key( "NORMALIZE" )
+				.name( "Normalize" )
+				.help( "Normalize intensities in all channels." )
+				.defaultValue( true )
+				.get();
+
+		this.nIter = addIntParameter()
+				.key( "N_ITER" )
+				.name( "N iterations" )
+				.help( "Number of iterations for dynamics computation. "
+						+ "If 0, it is set proportional to the diameter. " )
+				.defaultValue( 0 )
+				.min( 0 )
+				.get();
+
+		this.tileOverlap = addDoubleParameter()
+				.key( "TILE_OVERLAP" )
+				.name( "Tile overlap" )
+				.help( "<html>Fraction of overlap of tiles.</html>" )
+				.defaultValue( 0.1 )
+				.min( 0. )
+				.get();
+
+		addGroup( "Advanced parameters" )
+				.add( flowThreshold )
+				.add( cellprobThreshold )
+				.add( minSize )
+				.add( normalize )
+				.add( nIter )
+				.add( tileOverlap )
+				.collapsed( true )
+				.get();
+
+		/*
+		 * 3D group.
+		 */
+
+		this.do3Dseg = addBooleanParameter()
+				.key( "MODE_3D" )
+				.name( "3D segmentation" )
+				.help( "<html>How to handle 3D images for segmentation. "
+						+ "If set, will use Cellpose 3D segmentation. "
+						+ "Otherwise, segment each 2D plane and stitch objects in Z. See the 'Stitch threshold' parameter."
+						+ "</html>" )
+				.defaultValue( false )
+				.get();
+
+		this.stitchThreshold = addDoubleParameter()
+				.key( "STITCH_THRESHOLD" )
+				.name( "Stitch threshold" )
+				.help( "<html>When in 2D+stitch 3D mode, this threshold is used to decide whether to stitch "
+						+ "two objects across planes. Lower = more stitching (permissive), "
+						+ "Higher = less stitching (strict).</html>" )
+				.defaultValue( 0. )
+				.min( 0. )
+				.max( 1. )
+				.get();
+
+		this.flow3DSmooth = addIntParameter()
+				.key( "FLOW_3D_SMOOTH" )
+				.name( "3D flow smoothing" )
+				.help( "<html>When in 3D mode, smooth flows with gaussian filter of this sigma. "
+						+ "Helps smoothing objects masks in 3D. Set to 0 to disable smoothing.</html>" )
+				.defaultValue( 0 )
+				.min( 0 )
+				.get();
+
+		addGroup( "3D options" )
+				.add( do3Dseg )
+				.add( stitchThreshold )
+				.add( flow3DSmooth )
+				.collapsed( true )
+				.get();
+
+		/*
+		 * Export group.
+		 */
+
+		this.exportROIs = addBooleanParameter()
+				.key( "EXPORT_ROIS" )
+				.name( "Export ROIs" )
+				.help( "If set, ROIs will be computed from the labels output and added to the input image." )
+				.defaultValue( true )
+				.get();
+
+		this.exportLabels = addBooleanParameter()
+				.key( "EXPORT_LABELS" )
+				.name( "Export label image" )
+				.help( "If set, the label image will be shown." )
+				.defaultValue( false )
+				.get();
+
+		this.exportFlows = addBooleanParameter()
+				.key( "EXPORT_FLOWS" )
+				.name( "Export flows" )
+				.help( "If set, the Cellpose flows will be shown as a 3-channel image" )
+				.defaultValue( false )
+				.get();
+
+		addGroup( "Export options" )
+				.add( exportROIs )
+				.add( exportLabels )
+				.add( exportFlows )
+				.collapsed( false )
+				.get();
+
+		/*
+		 * GPU stuff.
+		 */
+
+		this.useGpu = addBooleanParameter()
+				.key( "USE_GPU" )
+				.name( "Use GPU" )
+				.help( "If set, Cellpose will try to use the GPU. If not available, it will fallback to CPU." )
+				.defaultValue( true )
+				.get();
+
+		this.torchVersion = addChoiceParameter()
+				.key( "TORCH_VERSION" )
+				.name( "Torch version" )
+				.help( "On Windows and Linux, control which torch / cuda version to use." )
+				.addChoice( "cpu" )
+				.addChoice( "cu128" )
+				.addChoice( "cu130" )
+				.defaultValue( "cpu" )
+				.get();
+
+		addGroup( "GPU options" )
+				.add( useGpu )
+				.add( torchVersion )
+				.collapsed( true )
+				.get();
+	}
+
+	public EnumParam< Cellpose3BuiltinModels > builtinModel()
+	{
+		return builtinModel;
+	}
+
+	public PathParam customModel()
+	{
+		return customModel;
+	}
+
+	public SelectableParameters builtinOrCustom()
+	{
+		return builtinOrCustom;
+	}
+
+
+	public IntParam chan1()
+	{
+		return chan1;
+	}
+
+	public IntParam chan2()
+	{
+		return chan2;
+	}
+
+	public DoubleParam diameter()
+	{
+		return diameter;
+	}
+
+	public DoubleParam flowThreshold()
+	{
+		return flowThreshold;
+	}
+
+	public DoubleParam cellprobThreshold()
+	{
+		return cellprobThreshold;
+	}
+
+	public IntParam minSize()
+	{
+		return minSize;
+	}
+
+	public BooleanParam exportROIs()
+	{
+		return exportROIs;
+	}
+
+	public BooleanParam exportLabels()
+	{
+		return exportLabels;
+	}
+
+	public BooleanParam exportFlows()
+	{
+		return exportFlows;
+	}
+
+	public BooleanParam normalize()
+	{
+		return normalize;
+	}
+
+	public IntParam nIter()
+	{
+		return nIter;
+	}
+
+	public BooleanParam mode3D()
+	{
+		return do3Dseg;
+	}
+
+	public DoubleParam stitchThreshold()
+	{
+		return stitchThreshold;
+	}
+
+	public IntParam flow3DSmooth()
+	{
+		return flow3DSmooth;
+	}
+
+	public DoubleParam tileOverlap()
+	{
+		return tileOverlap;
+	}
+
+	public BooleanParam useGpu()
+	{
+		return useGpu;
+	}
+
+	public ChoiceParam torchVersion()
+	{
+		return torchVersion;
+	}
+}
